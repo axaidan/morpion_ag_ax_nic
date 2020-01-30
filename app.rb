7c3acd389785ba6eabf15386e7e65ab7d0babb40
axaidan <axaidan@hotmail.com>
2020-01-30 12:32:42 +0100

    test creation user dans app.rb

--- app.rb
+++ app.rb
@@ -7,3 +7,7 @@ require_relative 'lib/app/board'
 require_relative 'lib/app/board_case'
 require_relative 'lib/app/application'
 require_relative 'lib/views/show'
+
+user1 = Player.new
+
+binding.pry
