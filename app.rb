062c152a653f38481b5ad36518ac810ad7a2cde9
axaidan <axaidan@hotmail.com>
2020-01-30 12:19:54 +0100

    init dossier

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require 
+
+require_relative 'lib/app/game'
+require_relative 'lib/app/player'
+require_relative 'lib/app/board'
+require_relative 'lib/app/board_case'
+require_relative 'lib/app/application'
+require_relative 'lib/views/show'
