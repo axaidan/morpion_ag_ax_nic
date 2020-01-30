84bbaac452cf998da96b55db5d6fc5f03d0ebd0a
axaidan <axaidan@hotmail.com>
2020-01-30 22:19:44 +0100

    morpio quasi def dans app.rb et game.rb

--- app.rb
+++ app.rb
@@ -8,20 +8,20 @@ require_relative 'lib/app/board_case'
 require_relative 'lib/app/application'
 require_relative 'lib/views/show'
 
-
-puts "Entrer le nom du joueur n1 :".light_yellow.italic
-name1 = gets.chomp.upcase
-puts "Entrer le nom du joueur n2 :".light_yellow.italic
-name2 = gets.chomp.upcase
-
-game = Game.new(name1, name2)
-
-# session = Application.new
-# session.welcome_screen
-# session.end_screen
+system("clear")
+puts "\n\n"
 
 screen = Show.new
 screen.welcome_screen
+
+puts "Entrer le nom du joueur n1 :".light_yellow.italic
+print "> "
+name1 = gets.chomp.upcase
+puts "Entrer le nom du joueur n2 :".light_yellow.italic
+print "> "
+name2 = gets.chomp.upcase
+
+game = Game.new(name1, name2)
 
 party = Game.new(name1, name2)
 party.play
